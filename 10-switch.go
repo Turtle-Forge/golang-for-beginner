70de7aac8b45c6fd34e73693482b1d6698f811e8
Benony Gabriel <gbenn.work@gmail.com>
2025-08-05 20:20:06 +0700

    swithc

---
+++ 10-switch.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	day := 3
+	switch day {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	case 4:
+		fmt.Println("Thursday")
+	case 5:
+		fmt.Println("Friday")
+	case 6:
+		fmt.Println("Saturday")
+	case 7:
+		fmt.Println("Sunday")
+	default:
+		fmt.Println("Invalid day")
+	}
+}
+
+// Switch statement example
+// This program uses a switch statement to print the name of the day based on the number provided
